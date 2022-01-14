f7e312d52b50cce0f3061128969911106a5f3355
Atsu77 <kamnknon@gmail.com>
2022-01-14 17:30:32 +0900

    いいね機能を追加

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', controllers: {
     registrations: 'auth/registrations'
   }
-  resources :messages, only: %i(index)
+  resources :messages, only: %i(index) do
+    member do
+      resources :likes, only: %i(create)
+    end
+  end
+  resources :likes, only: %i(destroy)
 end
